5b22f48f10c5d47d972baafacc4c6433abe89858
edghiriti <102817910+edghiriti@users.noreply.github.com>
2023-12-17 11:02:32 +0200

    Add project files.

---
+++ AboutPage.xaml.cs
@@ -0,0 +1,9 @@
+namespace Ghiriti_Edmond_Lab7;
+
+public partial class AboutPage : ContentPage
+{
+	public AboutPage()
+	{
+		InitializeComponent();
+	}
+}
---
+++ App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace Ghiriti_Edmond_Lab7;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
